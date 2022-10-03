54896bd380151e81590a979453b0928898967507
f3kilo <f3kilo@yandex.ru>
2022-10-03 15:10:09 +0300

    first implementation

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+const SHOW_NOTES: u8 = 1;
+const ADD_NOTE: u8 = 2;
+
+
+fn main() {
+    let mut notes = Vec::new();
+    loop {
+        show_menu();
+        let selected = read_menu_input();
+
+        match selected {
+            SHOW_NOTES => show_notes(&notes),
+            ADD_NOTE => notes.push(add_note()),
+            _ => break
+        }
+    }
+}
+
+fn show_menu() {
+    println!();
+    println!();
+    println!("**** MENU ****");
+    println!("1) show notes");
+    println!("2) add note");
+    println!("0) Exit");
+}
+
+fn read_menu_input() -> u8 {
+    let mut buf = String::new();
+    let stdin = std::io::stdin();
+    let read_result = stdin.read_line(&mut buf);
+    read_result.unwrap();
+    buf.trim().parse().unwrap()
+}
+
+fn show_notes(notes: &Vec<String>) {
+    println!();
+    println!("Notes list:");
+    for note in notes {
+        println!("{}", note)
+    }
+    
+}
+
+fn add_note() -> String {
+    println!();
+    println!("Enter new note:");
+    let mut buf = String::new();
+    let stdin = std::io::stdin();
+    let read_result = stdin.read_line(&mut buf);
+    read_result.unwrap();
+    buf.trim().to_string()
+}
